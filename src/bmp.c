5743928fb766920530dc36a3caaa0ea38f66cada
TretasPt <86566152+TretasPt@users.noreply.github.com>
2025-03-11 20:59:01 +0000

    Minimal working version before color.

--- src/bmp.c
+++ src/bmp.c
@@ -2,20 +2,29 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#pragma pack(push, 1) // Ensure no padding
+struct
+{
+    uint8_t blue;
+    uint8_t green;
+    uint8_t red;
+} Color;
+
 // Define the BMP header structures
 
 // BMP file header (14 bytes)
-#pragma pack(push, 1) // Ensure no padding
-typedef struct {
-    uint16_t bfType;         // File type, must be 'BM'
-    uint32_t bfSize;         // Size of the BMP file
-    uint16_t bfReserved1;    // Reserved, should be 0
-    uint16_t bfReserved2;    // Reserved, should be 0
-    uint32_t bfOffBits;      // Offset to the start of pixel data
+typedef struct
+{
+    uint16_t bfType;      // File type, must be 'BM'
+    uint32_t bfSize;      // Size of the BMP file
+    uint16_t bfReserved1; // Reserved, should be 0
+    uint16_t bfReserved2; // Reserved, should be 0
+    uint32_t bfOffBits;   // Offset to the start of pixel data
 } BMPHeader;
 
 // BMP info header (40 bytes)
-typedef struct {
+typedef struct
+{
     uint32_t biSize;         // Size of the info header
     int32_t biWidth;         // Width of the image
     int32_t biHeight;        // Height of the image
@@ -32,10 +41,12 @@ typedef struct {
 #pragma pack(pop)
 
 // Function to write BMP image
-void createBMP(const char *filename, int width, int height, uint8_t red, uint8_t green, uint8_t blue) {
+void createBMP(const char *filename, int width, int height, uint8_t red, uint8_t green, uint8_t blue)
+{
     // Create the BMP file
     FILE *file = fopen(filename, "wb");
-    if (!file) {
+    if (!file)
+    {
         perror("Unable to create BMP file");
         return;
     }
@@ -44,16 +55,16 @@ void createBMP(const char *filename, int width, int height, uint8_t red, uint8_t
     BMPHeader bmpHeader = {0};
     BMPInfoHeader bmpInfoHeader = {0};
 
-    bmpHeader.bfType = 0x4D42;  // "BM" in ASCII
+    bmpHeader.bfType = 0x4D42;                                                           // "BM" in ASCII
     bmpHeader.bfSize = sizeof(BMPHeader) + sizeof(BMPInfoHeader) + (width * height * 3); // Total file size
-    bmpHeader.bfOffBits = sizeof(BMPHeader) + sizeof(BMPInfoHeader); // Data offset
+    bmpHeader.bfOffBits = sizeof(BMPHeader) + sizeof(BMPInfoHeader);                     // Data offset
 
     bmpInfoHeader.biSize = sizeof(BMPInfoHeader);
     bmpInfoHeader.biWidth = width;
     bmpInfoHeader.biHeight = height;
     bmpInfoHeader.biPlanes = 1;
-    bmpInfoHeader.biBitCount = 24; // 24 bits for RGB
-    bmpInfoHeader.biCompression = 0; // No compression
+    bmpInfoHeader.biBitCount = 24;                  // 24 bits for RGB
+    bmpInfoHeader.biCompression = 0;                // No compression
     bmpInfoHeader.biSizeImage = width * height * 3; // Image data size
     // bmpInfoHeader.biXPelsPerMeter = 2835; // 72 DPI
     // bmpInfoHeader.biYPelsPerMeter = 2835; // 72 DPI
@@ -67,32 +78,46 @@ void createBMP(const char *filename, int width, int height, uint8_t red, uint8_t
     // Prepare pixel data (RGB)
     // uint32_t *pixelData = (uint32_t *)malloc(width * height * 4);
     uint8_t *pixelData = (uint8_t *)malloc(width * height * 3);
-    if (!pixelData) {
+    // struct Color *pixelData = (struct Color *)malloc(width * height * sizeof(Color));
+    if (!pixelData)
+    {
         perror("Unable to allocate memory for pixel data");
         fclose(file);
         return;
     }
 
     // Set the pixel data (all pixels will have the specified color)
-    for (int i = 0; i < width * height; i++) {
-        // pixelData[i] = 0xFFFFFF;   // Blue
-        pixelData[i * 3] = blue;   // Blue
-        pixelData[i * 3 + 1] = green; // Green
-        pixelData[i * 3 + 2] = red; // Red
-    }
+    for (int i = 0; i < width * height; i++)
+    {
+        // pixelData[i].red = 255;
+        // pixelData[i].green = 255;
+        // pixelData[i].blue = 255;
+    //     pixelData[i] = {
+    //         .red = 255;
+    //     .green = 255;
+    //     .blue = 255;
+    // }
 
-    // Write pixel data to the file
-    fwrite(pixelData, 1, width * height * 3, file);
-
-    // Clean up and close the file
-    free(pixelData);
-    fclose(file);
+    // Blue
+    pixelData[i * 3] = 255;     // blue;   // Blue
+    pixelData[i * 3 + 1] = 155; // green; // Green
+    pixelData[i * 3 + 2] = 55; // red; // Red
 }
 
-int main() {
-    int width = 100; // Image width
-    int height = 100; // Image height
+// Write pixel data to the file
+fwrite(pixelData, 1, width *height * 3, file);
+
+// Clean up and close the file
+free(pixelData);
+fclose(file);
+}
+
+int main()
+{
+    int width = 100;                        // Image width
+    int height = 100;                       // Image height
     uint8_t red = 255, green = 0, blue = 0; // Color (red)
+    // printf("Color size %d", sizeof(Color));
 
     // Create a BMP file with the specified dimensions and color
     createBMP("output.bmp", width, height, red, green, blue);
